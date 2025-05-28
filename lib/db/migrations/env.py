7dcf0f731d5b2c76f3de4bf4370d34e2c0428fee
Emmanuel Gitau <gmannu2055@gmail.com>
2025-05-28 12:40:02 +0300

    tried fixing SQLAlchemy

--- lib/db/migrations/env.py
+++ lib/db/migrations/env.py
@@ -2,10 +2,12 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
-
 from alembic import context
-
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from models import Base
+from models import patients
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
