04efd3f3b8882e8a5ee2aa9cb33d6dfd0b32b9b6
Emmanuel Gitau <gmannu2055@gmail.com>
2025-05-29 12:40:29 +0300

    fixed folder structure

--- lib/db/Models/__init__.py
+++
@@ -1,5 +0,0 @@
-# This file is part of the Health Management System project.
-#to make the code cleaner and more organized, we will import the models here
-from .doctor import Doctor
-# from .patient import Patient
-# from .appointment import Appointment
